9173ca1c5fca8371b45df62de717b24468288b7d
Andréas <andreas.appelqvist@gmail.com>
2016-09-23 22:11:09 +0200

    fixed so it works with uneven gameboards aswell

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,15 +1,10 @@
 package main;
-
 import java.util.ArrayList;
-
 /**
- *
  * Created by Andréas Appelqvist on 2016-09-22.
- *
  */
 
 public class Game {
-
     ArrayList<Brick> board;
     int totalRows, totalCols;
 
@@ -21,52 +16,39 @@ public class Game {
 
     private void initBoard(){
         board = new ArrayList<Brick>();
-
         for(int i = 0; i < totalCols*totalRows; i++){
             board.add(new Brick());
         }
 
-
-        //Just for even squares
-        int startCol = -1, startRow = -1;
-        if(totalCols % 2 == 0){
-            startCol = (totalCols/2)-1;
-        }else{
-            //Do not work with uneven squares yet
-        }
-
-        if(totalRows % 2 == 0){
-            startRow = (totalRows/2)-1;
-        }else{
-            //Do not work with uneven squares yet
-        }
+        int startCol, startRow;
+        startCol = (int)(totalCols/2)-1;
+        startRow = (int)(totalRows/2)-1;
 
         board.get((startRow*totalCols)+startCol).setWhite();
         board.get((startRow*totalCols)+startCol+1).setBlack();
         board.get((startRow*totalCols)+startCol+totalCols).setBlack();
         board.get((startRow*totalCols)+startCol+totalCols+1).setWhite();
 
-        printCurrentBoard();
+        System.out.println(getBoard());
     }
 
     public void startGame(){
-
     }
 
-    private void printCurrentBoard(){
-
+    private String getBoard(){
+        String str = "";
         for(int i = 0; i < totalRows; i ++){
             for(int j = 0; j < totalCols; j ++){
                 if(board.get((i*totalCols)+j).isBlack()){
-                    System.out.print("[B] ");
+                    str += "[B] ";
                 }else if(board.get((i*totalCols)+j).isWhite()){
-                    System.out.print("[W] ");
+                    str += "[W] ";
                 }else{
-                    System.out.print("[ ] ");
+                    str += "[ ] ";
                 }
             }
-            System.out.println();
+            str += "\n";
         }
+        return str;
     }
-
 }
