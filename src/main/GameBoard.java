59ec0ecb52bb00b37ea6f80e67a43e1c35b84a6b
Andréas <andreas.appelqvist@gmail.com>
2016-09-25 18:47:34 +0200

    legal moves on diagonal aswell

--- src/main/GameBoard.java
+++ src/main/GameBoard.java
@@ -27,11 +27,11 @@ public class GameBoard {
         startCol = (int) (totalCols / 2) - 1;
         startRow = (int) (totalRows / 2) - 1;
 
+
         board.get((startRow * totalCols) + startCol).setWhite();
         board.get((startRow * totalCols) + startCol + 1).setBlack();
         board.get((startRow * totalCols) + startCol + totalCols).setBlack();
         board.get((startRow * totalCols) + startCol + totalCols + 1).setWhite();
-
     }
 
     public LinkedList<Integer> getComputersLegalMove() {
@@ -53,40 +53,99 @@ public class GameBoard {
                         multiplier += totalRows;
                     }
                     if (i + multiplier < board.size() && player == board.get(i + multiplier).getCurrentTileInt()) {
-                        legalMoves.add(i);
+                        if (!legalMoves.contains(i))
+                            legalMoves.add(i);
                     }
-                } else if (i - totalRows >= 0 && player * -1 == board.get(i - totalRows).getCurrentTileInt()){ //Check tile over board(i)
+                }
+
+                //Check tile over board(i)
+                if (i - totalRows >= 0 && player * -1 == board.get(i - totalRows).getCurrentTileInt()) {
                     int multiplier = totalRows;
                     while (i - multiplier >= 0 && player * -1 == board.get(i - multiplier).getCurrentTileInt()) {
                         multiplier += totalRows;
                     }
                     if (i - multiplier >= 0 && player == board.get(i - multiplier).getCurrentTileInt()) {
-                        legalMoves.add(i);
+                        if (!legalMoves.contains(i))
+                            legalMoves.add(i);
                     }
-                    //Looking right of board(i)
-                }else if((i + 1) % totalCols > 0){  //If it's not a wall on right side
-                    if(i + 1 < board.size() && player * -1 == board.get(i+1).getCurrentTileInt()){
+                }
+
+                //Looking right of board(i)
+                if ((i + 1) % totalCols > 0) {  //If it's not a wall on right side
+                    if (i + 1 < board.size() && player * -1 == board.get(i + 1).getCurrentTileInt()) {
                         int increase = 1;
-                        while ( (i + increase)% totalCols > 0 && i + increase < board.size() && player * -1 == board.get(increase + i).getCurrentTileInt()){
-                            increase ++;
+                        while ((i + increase) % totalCols > 0 && i + increase < board.size() && player * -1 == board.get(increase + i).getCurrentTileInt()) {
+                            increase++;
                         }
-                        if((i + increase)% totalCols > 0 && i + increase < board.size() && player == board.get(increase + i).getCurrentTileInt()){
-                            legalMoves.add(i);
+                        if ((i + increase) % totalCols > 0 && i + increase < board.size() && player == board.get(increase + i).getCurrentTileInt()) {
+                            if (!legalMoves.contains(i))
+                                legalMoves.add(i);
                         }
                     }
-                    //Looking left of board(i)
-                }else if(i % totalCols > 0){ //If it's not a wall on left side of board(i)
-                    if(i-1 >= 0 && !board.get(i-1).isEmpty() && player * -1 == board.get(i-1).getCurrentTileInt()){
-                        int increse = 1;
-                        while( i - increse >= 0 && (i - increse) % totalCols > 0 && player * -1 == board.get(i -increse).getCurrentTileInt()){
-                            increse++;
+
+                    //Looking SouthEast
+                    if (i + totalRows + 1 < board.size() && player * -1 == board.get(i + totalRows + 1).getCurrentTileInt()) {
+                        int increase = totalRows + 1;
+                        while (i + increase % totalCols > 0 && increase + i < board.size() && player * -1 == board.get(i + increase).getCurrentTileInt()) {
+                            increase += (totalRows + 1);
                         }
-                        if(i - increse >= 0 && (i - increse) % totalCols > 0 && player == board.get(i -increse).getCurrentTileInt()){
-                            legalMoves.add(i);
+                        if (i + increase % totalCols > 0 && increase + i < board.size() && player == board.get(i + increase).getCurrentTileInt()) {
+                            if (!legalMoves.contains(i)) {
+                                legalMoves.add(i);
+                            }
+                        }
+                    }
+
+                    //Looking NorthEast
+                    if (i + 1 - totalRows >= 0 && player * -1 == board.get(i + 1 - totalRows).getCurrentTileInt()) {
+                        int increase = totalRows - 1;
+                        while (i - increase % totalCols > 0 && i - increase >= 0 && player * -1 == board.get(i - increase).getCurrentTileInt()) {
+                            increase += (totalRows - 1);
+                        }
+                        if (i - increase % totalCols > 0 && i - increase >= 0 && player == board.get(i - increase).getCurrentTileInt()) {
+                            if (!legalMoves.contains(i))
+                                legalMoves.add(i);
                         }
                     }
                 }
 
+                //Looking left of board(i)
+                if (i % totalCols > 0) { //If it's not a wall on left side of board(i)
+                    if (i - 1 >= 0 && player * -1 == board.get(i - 1).getCurrentTileInt()) {
+                        int increse = 1;
+                        while (i - increse >= 0 && (i - increse) % totalCols > 0 && player * -1 == board.get(i - increse).getCurrentTileInt()) {
+                            increse++;
+                        }
+                        if (i - increse >= 0 && (i - increse) % totalCols > 0 && player == board.get(i - increse).getCurrentTileInt()) {
+                            if (!legalMoves.contains(i))
+                                legalMoves.add(i);
+                        }
+                    }
+
+                    //Looking SouthWest
+                    if (i + totalCols - 1 < board.size() && player * -1 == board.get(i + totalCols - 1).getCurrentTileInt()) {
+                        int increase = totalCols - 1;
+                        while (i + increase < board.size() && i + increase % totalCols > 0 && player * -1 == board.get(i + increase).getCurrentTileInt()) {
+                            increase += (totalCols - 1);
+                        }
+                        if (i + increase < board.size() && i + increase % totalCols > 0 && player == board.get(i + increase).getCurrentTileInt()) {
+                            if (!legalMoves.contains(i))
+                                legalMoves.add(i);
+                        }
+                    }
+
+                    //Looking NothWest
+                    if (i - totalCols - 1 >= 0 && player * -1 == board.get(i - 1 - totalCols).getCurrentTileInt()) {
+                        int increase = (totalCols + 1);
+                        while (i - increase >= 0 && i - increase % totalCols > 0 && player * -1 == board.get(i - increase).getCurrentTileInt()) {
+                            increase += totalCols + 1;
+                        }
+                        if (i - increase >= 0 && i - increase % totalCols > 0 && player == board.get(i - increase).getCurrentTileInt()) {
+                            if (!legalMoves.contains(i))
+                                legalMoves.add(i);
+                        }
+                    }
+                }
             }
         }
         return legalMoves;
